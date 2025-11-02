3f2dde83d58e8a665c7f04fcc65d1aad60edae71
xmcnulty <6352496+xmcnulty@users.noreply.github.com>
2025-11-02 14:59:15 -0800

    basic exit functionality

--- app/main.py
+++ app/main.py
@@ -5,8 +5,16 @@ def main():
     while True:
         sys.stdout.write("$ ")
         
-        command = input()
-        print(f"{command}: command not found")
+        command = input().split()
+
+        if not command:
+            print("please enter a command")
+            continue
+
+        if command[0] == "exit":
+            return int(command[1]) if len(command) > 1 else 0
+
+        print(f"{command[0]}: command not found")
 
 
 if __name__ == "__main__":
