c5f43b33c56320dc610aa6212f93aea3fe85a59d
xmcnulty <6352496+xmcnulty@users.noreply.github.com>
2025-11-02 15:11:58 -0800

    basic echo implementation

--- app/main.py
+++ app/main.py
@@ -5,16 +5,22 @@ def main():
     while True:
         sys.stdout.write("$ ")
         
-        command = input().split()
+        command = input()
 
         if not command:
             print("please enter a command")
             continue
 
-        if command[0] == "exit":
-            return int(command[1]) if len(command) > 1 else 0
+        if command.split()[0] == "exit":
+            args = command.split()
 
-        print(f"{command[0]}: command not found")
+            return int(args[1]) if len(args) > 1 else 0
+
+        if command.split()[0] == "echo":
+            print(command[4:].lstrip())
+            continue
+
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
