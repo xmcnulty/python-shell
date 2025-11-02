af32d8432d1b6eb9684d9d014f79b6616dbbff3b
xmcnulty <6352496+xmcnulty@users.noreply.github.com>
2025-11-02 14:51:45 -0800

    second stage

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
