ca058a6de262d13fb8d0317ab7f5531f0a1c56e9
xmcnulty <6352496+xmcnulty@users.noreply.github.com>
2025-11-02 15:36:41 -0800

    implemented basic type command functionality

--- app/main.py
+++ app/main.py
@@ -1,26 +1,44 @@
 import sys
+from typing import Optional
 
+def echo(args: Optional[str]):
+    if args:
+        print(args.lstrip())
+
+def type_cmd(args: Optional[str]):
+    if args:
+        if args in commands.keys():
+            print(f"{args} is a shell builtin")
+        else:
+            print(f"{args}: not found")
+
+commands = {
+    "exit" : None, 
+    "echo" : echo, 
+    "type" : type_cmd
+}
 
 def main():
     while True:
         sys.stdout.write("$ ")
         
         command = input()
-
+        
         if not command:
             print("please enter a command")
             continue
 
-        if command.split()[0] == "exit":
-            args = command.split()
+        elements = command.split(maxsplit=1)
+        cmd = elements[0]
+        args = elements[1] if len(elements) == 2 else None
 
-            return int(args[1]) if len(args) > 1 else 0
+        if cmd == "exit":
+            return int(args) if args and args.isnumeric() else 0
 
-        if command.split()[0] == "echo":
-            print(command[4:].lstrip())
-            continue
-
-        print(f"{command}: command not found")
+        if cmd in commands.keys():
+            commands[cmd](args)
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
